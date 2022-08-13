f444c8cce270cf59d2cd0ef7c969445199005ca2
mcguiretj18 <mcguiretj18@gmail.com>
2022-08-13 02:46:10 -0400

    fix accordion click and styling

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const h2Elements = document.querySelectorAll('h2');
+const accordions = document.querySelectorAll('.accordion');
 
 const clickHandler = (elements, index) => (event) => {
-    const accordionNode = elements[index].parentElement.parentElement;
+    const accordionNode = elements[index];
     const classes = [...accordionNode.classList];
     // If current accordion is open, close it
     if (classes.includes("show")) {
         accordionNode.classList.replace("show", "hidden");
         elements.forEach(element => {
-            element.parentElement.parentElement.classList.add('hidden');
+            element.classList.add('hidden');
         });
     } else if (classes.includes("hidden")) {
         elements.forEach(element => {
-            element.parentElement.parentElement.classList.remove("show");
-            element.parentElement.parentElement.classList.add("hidden");
+            element.classList.remove("show");
+            element.classList.add("hidden");
         });
         accordionNode.classList.replace("hidden", "show");
     }
 }
 
-h2Elements.forEach((element, index) => {
-    element.addEventListener('click', clickHandler(h2Elements, index));
+accordions.forEach((element, index) => {
+    element.addEventListener('click', clickHandler(accordions, index));
 })
